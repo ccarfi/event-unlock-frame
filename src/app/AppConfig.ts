4b9ea20ad7d781fde0f4bd31e25a754b1fcc018e
Christopher Carfi <ccarfi@gmail.com>
2024-02-01 20:49:02 -0800

    Update AppConfig.ts
    Update the name and description

--- src/app/AppConfig.ts
+++ src/app/AppConfig.ts
@@ -1,6 +1,6 @@
 export const AppConfig = {
-  name: "Token Gated Frames",
-  description: `A simple Farcaster Frame application that lets you create a token-gated cast. `,
+  name: "Event Unlock Frame",
+  description: `A simple Farcaster Frame application that lets you register for an event. `,
   environment: process.env.NEXT_PUBLIC_VERCEL_ENV,
   siteUrl: process.env.NEXT_PUBLIC_URL_BASE || "http://localhost:3000",
   googleAnalyticsId: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID!,
